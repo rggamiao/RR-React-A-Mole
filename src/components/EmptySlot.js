9b1398fdbbdf48a46932346b9e9d31fe3b21a217
rggamiao <rejicamiling@gmail.com>
2024-06-07 13:26:24 -0700

    First commit

--- src/components/EmptySlot.js
+++ src/components/EmptySlot.js
@@ -1,21 +1,20 @@
-import { useEffect } from "react"
-import MoleHill from '../molehill.png'
+import { useEffect } from "react";
+import moleHillImage from "./path/to/mole-hill-image.jpg";
 
 const EmptySlot = (props) => {
+  useEffect(() => {
+    let randSeconds = Math.ceil(Math.random() * 5000);
+    let timer = setTimeout(() => {
+      props.toggle(true);
+    }, randSeconds);
+    return () => clearTimeout(timer);
+  }, []);
 
-    useEffect(() => {
-        let randSeconds = Math.ceil(Math.random() * 5000)
-        let timer = setTimeout(() => {
-            props.toggle(true)
-        }, randSeconds)
-        return () => clearTimeout(timer)
-    })
+  return (
+    <div>
+      <img style={{ width: "30vw" }} src={moleHillImage} alt="Mole Hill" />
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <img style={{'width': '30vw'}} src={MoleHill} />
-        </div>
-    )
-}
-
-export default EmptySlot
+export default EmptySlot;
--- src/components/Mole.js
+++ src/components/Mole.js
@@ -1,21 +1,20 @@
-import { useEffect } from 'react'
-import moleImg from '../mole.png'
+import React, { useEffect } from 'react';
+import moleImg from '../mole.png';
 
 const Mole = (props) => {
-
     useEffect(() => {
-        let randSeconds = Math.ceil(Math.random() * 10000)
+        let randSeconds = Math.ceil(Math.random() * 10000);
         let timer = setTimeout(() => {
-            props.toggle(false)
-        }, randSeconds)
-        return () => clearTimeout(timer)
-    })
+            props.toggle(false);
+        }, randSeconds);
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
         <div>
-            <img style={{'width': '30vw'}} src={moleImg} onClick={props.handleClick} />
+            <img style={{ width: '30vw', cursor: 'pointer' }} src={moleImg} alt="Mole" onClick={props.handleClick} />
         </div>
-    )
-}
+    );
+};
 
-export default Mole
+export default Mole;
